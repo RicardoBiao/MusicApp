b46eb054079a2584a8e47e32ddff2cfe04e30381
Ricardo <532569829@qq.com>
2022-11-15 11:53:04 +0800

    chore: 调整目录

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,16 +1,23 @@
+<!--
+ * @Author: liweibiao
+ * @Date: 2022-11-15 10:42:32
+ * @LastEditTime: 2022-11-15 11:52:21
+ * @LastEditors: liweibiao
+ * @Description: 
+-->
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './shared/components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="../../public/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 /*
  * @Author: liweibiao
  * @Date: 2022-11-15 10:42:32
- * @LastEditTime: 2022-11-15 10:56:29
+ * @LastEditTime: 2022-11-15 11:49:37
  * @LastEditors: liweibiao
  * @Description: 
  */
 
 import { createApp } from 'vue'
 import './style/style.scss'
-import App from './App.vue'
+import App from './app/App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.mount('#app')
